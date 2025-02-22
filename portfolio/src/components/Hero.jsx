879f47d5f36520e575e2e7d01ca8e71b078327f7
enjiwood <87221016+enjiwood@users.noreply.github.com>
2025-02-21 17:47:53 -0800

    Add some styling to Hero, About, and begin Projects section

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,14 +1,12 @@
 const Hero = () => {
     return (
-        <div id="hero" className="section p-20">
-            <header id="header">
-				<div class="inner">
-					<img className="w-30 h-30 rounded-full" src="/images/avatar.jpg" alt="Profile Photo"/>
-					<h1><strong>Nathaniel G. Wood</strong></h1>
-					<h3>OSU computer science student and aspiring software engineer.</h3>
-				</div>
-			</header>
-        </div>
+        <section className="min-h-screen w-full flex flex-col relative">
+			<div className="w-full mx-auto flex flex-col gap-3 my-auto items-center">
+				<img className="w-30 h-30 rounded-full mx-auto border-(--color) border-4" src="/images/avatar.jpg" alt="Profile Photo"/>
+				<h1 className="sm:text-3xl text-2xl"><strong>Nathaniel G. Wood</strong></h1>
+				<h3 className="hero_tag text-(--color)_gradient">OSU computer science student and aspiring software developer.</h3>
+			</div>
+		</section>
     );
 };
 
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,9 +1,14 @@
 const Projects = () => {
     return (
-        <div id="projects" className="section">
-            <h1>Projects</h1>
-            <p>I have quite a few projects under my belt.</p>
-        </div>
+        <section className="flex flex-col items-center c-space my-10">
+            <h1 className="mb-10">Projects</h1>
+            <div className="grid xl:grid-cols-3 xl:grid-rows-6 grid-cols-1 mx-auto w-full items-center gap-3 h-full">
+                <div className="col-span-1 xl:row-span-2 bg-[#f9f9f9] dark:bg-[#1a1a1a] xl:rounded-2xl rounded-xl
+                    h-auto shadow-sm shadow-gray-500/50 dark:shadow-[#000000] gap-3 p-3 items-center hover:bg-gradient-to-b from-[#f9f9f9] to-[#e6e6e6] dark:from-[#1a1a1a] dark:to-[#151515]">
+                    <h2 className="text-xl text-(--color)">Project 1</h2>
+                </div>
+            </div>
+        </section>
     );
 };
 
