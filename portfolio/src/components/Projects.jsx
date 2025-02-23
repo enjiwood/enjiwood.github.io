06bdee7d25378c79f411d2b82417da4bfc2c84f5
enjiwood <87221016+enjiwood@users.noreply.github.com>
2025-02-22 16:45:28 -0800

    Fix side bar

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -4,8 +4,28 @@ const Projects = () => {
             <h1 className="mb-10">Projects</h1>
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 grid-cols-1 mx-auto w-full items-center gap-3 h-full">
                 <div className="col-span-1 xl:row-span-2 bg-[#f9f9f9] dark:bg-[#1a1a1a] xl:rounded-2xl rounded-xl
-                    h-auto shadow-sm shadow-gray-500/50 dark:shadow-[#000000] gap-3 p-3 items-center duration-300 hover:scale-102 from-[#f9f9f9] to-[#e6e6e6] dark:from-[#1a1a1a] dark:to-[#151515]">
-                    <h2 className="text-xl text-(--color)">Project 1</h2>
+                    h-auto shadow-sm shadow-gray-500/50 dark:shadow-[#000000] gap-3 p-3 items-center duration-300 hover:scale-102 hover:shadow-lg from-[#f9f9f9] to-[#e6e6e6] dark:from-[#1a1a1a] dark:to-[#151515]">
+                    <h2 className="text-xl text-(--color)">Open Response</h2>
+                </div>
+                <div className="col-span-1 xl:row-span-2 bg-[#f9f9f9] dark:bg-[#1a1a1a] xl:rounded-2xl rounded-xl
+                    h-auto shadow-sm shadow-gray-500/50 dark:shadow-[#000000] gap-3 p-3 items-center duration-300 hover:scale-102 hover:shadow-lg from-[#f9f9f9] to-[#e6e6e6] dark:from-[#1a1a1a] dark:to-[#151515]">
+                    <h2 className="text-xl text-(--color)">Habit Tracker</h2>
+                </div>
+                <div className="col-span-1 xl:row-span-2 bg-[#f9f9f9] dark:bg-[#1a1a1a] xl:rounded-2xl rounded-xl
+                    h-auto shadow-sm shadow-gray-500/50 dark:shadow-[#000000] gap-3 p-3 items-center duration-300 hover:scale-102 hover:shadow-lg from-[#f9f9f9] to-[#e6e6e6] dark:from-[#1a1a1a] dark:to-[#151515]">
+                    <h2 className="text-xl text-(--color)">Tracks</h2>
+                </div>
+                <div className="col-span-1 xl:row-span-2 bg-[#f9f9f9] dark:bg-[#1a1a1a] xl:rounded-2xl rounded-xl
+                    h-auto shadow-sm shadow-gray-500/50 dark:shadow-[#000000] gap-3 p-3 items-center duration-300 hover:scale-102 hover:shadow-lg from-[#f9f9f9] to-[#e6e6e6] dark:from-[#1a1a1a] dark:to-[#151515]">
+                    <h2 className="text-xl text-(--color)">Voxelization GLSL Shader</h2>
+                </div>
+                <div className="col-span-1 xl:row-span-2 bg-[#f9f9f9] dark:bg-[#1a1a1a] xl:rounded-2xl rounded-xl
+                    h-auto shadow-sm shadow-gray-500/50 dark:shadow-[#000000] gap-3 p-3 items-center duration-300 hover:scale-102 hover:shadow-lg from-[#f9f9f9] to-[#e6e6e6] dark:from-[#1a1a1a] dark:to-[#151515]">
+                    <h2 className="text-xl text-(--color)">Smallsh</h2>
+                </div>
+                <div className="col-span-1 xl:row-span-2 bg-[#f9f9f9] dark:bg-[#1a1a1a] xl:rounded-2xl rounded-xl
+                    h-auto shadow-sm shadow-gray-500/50 dark:shadow-[#000000] gap-3 p-3 items-center duration-300 hover:scale-102 hover:shadow-lg from-[#f9f9f9] to-[#e6e6e6] dark:from-[#1a1a1a] dark:to-[#151515]">
+                    <h2 className="text-xl text-(--color)">Mobile Treasure Hunt</h2>
                 </div>
             </div>
         </section>
--- portfolio/src/components/SideBar.jsx
+++ portfolio/src/components/SideBar.jsx
@@ -20,15 +20,18 @@ const SideBar = () => {
     };
 
     return (
-        <div className="h-full w-auto">
+        <div className="h-full w-auto lg:ml-10 sm:ml-5">
             <div className="sticky top-20 h-[80vh] py-32 flex gap-4">
-                <div className="hidden lg:flex flex-col gap-6 text-sm xl:text-base">
-                    <span onClick={heroSection}>Home</span>
-                    <span onClick={aboutSection}>About Me</span>
+                <div className="h-full flex items-center">
+                    <div className="hidden lg:flex flex-col h-full justify-evenly gap-6 text-sm xl:text-base">
+                        <span className="text-right" onClick={heroSection}>Home</span>
+                        <span className="text-right" onClick={aboutSection}>About Me</span>
+                        <span className="text-right" onClick={projectsSection}>Projects</span>
+                        <span className="text-right" onClick={experienceSection}>Experience</span>
+                        <span className="text-right" onClick={contactSection}>Contact</span>
+                    </div>
                 </div>
-                <div className="hidden lg:flex h-full w-0.5 bg-[#213547] dark:bg-[#ffffffde] rounded-full overflow-hidden">
-
-                </div>
+                <div className="hidden lg:flex h-full w-0.5 bg-[#213547] dark:bg-[#ffffffde] rounded-full overflow-hidden"/>
             </div>
         </div>
     );
